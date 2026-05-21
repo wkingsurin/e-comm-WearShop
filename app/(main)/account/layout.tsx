6cf16f540a1ed0af31d493d2cf7e356b2ba2f209
Daniil Surin <wkingsurin@gmail.com>
2026-05-21 14:11:49 +0500

    feat(pages): create Account page and route components
    - Created route components: Profile, Orders, Purchases, Favourites,
      Security

---
+++ app/(main)/account/layout.tsx
@@ -0,0 +1,90 @@
+import Main from "@/components/main";
+import Container from "@/components/shared/container";
+import Section from "@/components/shared/section";
+import SectionTitle from "@/components/shared/section-title";
+import SortSelect from "@/components/shared/sort-select";
+import ProductCard from "@/components/widgets/product-card";
+import {
+	Boxes,
+	Heart,
+	Lock,
+	LogOut,
+	LucideIcon,
+	Package,
+	User,
+} from "lucide-react";
+import Link from "next/link";
+
+interface IProps {
+	children: React.ReactNode;
+}
+
+export default function AccountLayout({ children }: IProps) {
+	const navigationData: {
+		id: string;
+		label: string;
+		ref: string;
+		icon: LucideIcon;
+	}[] = [
+		{ id: "1", label: "Profile", ref: "/", icon: User },
+		{ id: "2", label: "Orders", ref: "orders", icon: Package },
+		{ id: "3", label: "Purchases", ref: "purchases", icon: Boxes },
+		{ id: "4", label: "Favourites", ref: "favourites", icon: Heart },
+		{ id: "5", label: "Security", ref: "security", icon: Lock },
+		{ id: "6", label: "Log out", ref: "/", icon: LogOut },
+	];
+
+	return (
+		<Main>
+			<Section>
+				<Container>
+					<div className="flex flex-col gap-5">
+						<div className="flex items-center justify-between">
+							<SectionTitle>Account</SectionTitle>
+						</div>
+						<div className="flex gap-[60px]">
+							<nav className="max-w-[305px] w-full">
+								{navigationData.map((item) => {
+									const Icon = item.icon;
+
+									return (
+										<Link
+											href={`/account/${item.ref}`}
+											key={item.id}
+											className="flex items-center gap-3 h-10 px-3 rounded-xl bg-transparent hover:bg-black/10 transition duration-100"
+										>
+											<Icon className="size-5 stroke-[1.5px] stroke-black" />
+											<p className="font-mono tracking-wide">{item.label}</p>
+										</Link>
+									);
+								})}
+							</nav>
+							<div>{children}</div>
+						</div>
+					</div>
+				</Container>
+			</Section>
+			<Section>
+				<Container>
+					<div className="flex flex-col gap-5">
+						<div className="flex flex-col gap-[6px]">
+							<span className="uppercase font-medium text-black/50 tracking-wider">
+								You See
+							</span>
+							<div className="flex items-center justify-between">
+								<SectionTitle>Last seen products</SectionTitle>
+								<SortSelect />
+							</div>
+						</div>
+						<div className="flex flex-wrap gap-5">
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+							<ProductCard />
+						</div>
+					</div>
+				</Container>
+			</Section>
+		</Main>
+	);
+}
