71ea6805546bc56d461b9e6f7776f405c67dddd0
Daniil Surin <wkingsurin@gmail.com>
2026-07-17 10:48:39 +0500

    feat(orders): add OrderReturn component

--- app/(main)/account/orders/[id]/client.tsx
+++ app/(main)/account/orders/[id]/client.tsx
@@ -4,7 +4,6 @@ import { useOrder } from "@/features/orders/hooks/use-order";
 import { EMPTY_ORDER } from "@/features/orders/constants";
 import { useRouter } from "next/navigation";
 import { useUIStore } from "@/lib/store/ui.store";
-import ShippingForm from "@/features/checkout/components/shipping-form/form";
 import DashboardWrapper from "@/components/shared/dashboard-wrapper";
 import { Button } from "@/components/ui/button";
 import { MoveLeft } from "lucide-react";
@@ -13,29 +12,13 @@ import OrderSummary from "@/features/orders/components/details/order-summary";
 import OrderReturn from "@/features/orders/components/details/order-return";
 import OrderConfirmModal from "@/features/orders/components/order-confirm-modal";
 import { useCancelOrder } from "@/features/orders/hooks/use-cancel-order";
+import { OrderStatus } from "@/prisma/generated/prisma/enums";
 
 export default function OrderPageClient({ orderId }: { orderId: string }) {
     const { data: order = EMPTY_ORDER } = useOrder(orderId);
     const { mutate: cancelOrder } = useCancelOrder();
 
-    const openDialog = useUIStore((s) => s.openDialog);
     const openConfirm = useUIStore((s) => s.openConfirm);
-
-    const handleEdit = () => {
-        openDialog({
-            title: "",
-            content: (
-                <ShippingForm
-                    initialValues={{
-                        country: order.shipping.city,
-                        city: order.shipping.city,
-                        address: order.shipping.address,
-                        postalCode: order.shipping.postalCode,
-                    }}
-                />
-            ),
-        });
-    };
 
     const handleCancel = () => {
         openConfirm({
@@ -75,7 +58,12 @@ export default function OrderPageClient({ orderId }: { orderId: string }) {
                     itemsAmount={order.items.length}
                     totals={order.totals}
                 />
-                <OrderReturn onEdit={handleEdit} onCancel={handleCancel} />
+                {order.status === OrderStatus.PENDING && (
+                    <OrderReturn
+                        initialValues={order.shipping}
+                        onCancel={handleCancel}
+                    />
+                )}
             </div>
         </div>
     );
--- features/orders/components/details/order-return.tsx
+++ features/orders/components/details/order-return.tsx
@@ -1,30 +1,48 @@
 import { Button } from "@/components/ui/button";
 import { Pencil } from "lucide-react";
+import { useUIStore } from "@/lib/store/ui.store";
+import ShippingForm from "@/features/checkout/components/shipping-form/form";
 
 interface IProps {
-    onEdit: () => void;
-    onCancel: () => void;
+    initialValues: {
+        country: string;
+        city: string;
+        address: string;
+        postalCode: string;
+    };
+    onCancel?: () => void;
 }
 
-export default function OrderReturn({ onEdit, onCancel }: IProps) {
+export default function OrderReturn({ initialValues, onCancel }: IProps) {
+    const openDialog = useUIStore((s) => s.openDialog);
+
+    const handleEdit = () => {
+        openDialog({
+            title: "Edit address",
+            content: <ShippingForm initialValues={initialValues} />,
+        });
+    };
+
     return (
         <div className="flex flex-col gap-4 bg-white rounded-xl p-6 hover:shadow-[0_0_12px_-3px_rgba(0,0,0,.1)] transition-brand">
             <Button
                 variant="link"
                 className="flex gap-3 justify-start w-full h-10 px-0 hover:no-underline hover:bg-[#F8F9FA]"
-                onClick={() => onEdit()}
+                onClick={handleEdit}
             >
                 <span className="flex items-center justify-center w-10 h-10 rounded-md bg-[#F8F9FA]">
                     <Pencil className="size-5 stroke-black/50" />
                 </span>
                 <p>Edit address</p>
             </Button>
-            <Button
-                className="h-[50px] bg-black/15 hover:bg-[#F51E1E]/10 hover:text-[#F51E1E]/75"
-                onClick={() => onCancel()}
-            >
-                Cancel order
-            </Button>
+            {onCancel !== undefined && (
+                <Button
+                    className="h-[50px] bg-black/15 hover:bg-[#F51E1E]/10 hover:text-[#F51E1E]/75"
+                    onClick={() => onCancel()}
+                >
+                    Cancel order
+                </Button>
+            )}
         </div>
     );
 }
