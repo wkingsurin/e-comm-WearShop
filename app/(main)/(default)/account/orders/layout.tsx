9c5e7ca02c7253df584e3549d19af35e811dbc37
Daniil Surin <wkingsurin@gmail.com>
2026-07-24 13:01:49 +0500

    feat(orders): lock OrderDetails page for unauthorized users

--- app/(main)/(default)/account/orders/layout.tsx
+++ app/(main)/(default)/account/orders/layout.tsx
@@ -1,11 +1,19 @@
 import BackButton from "@/components/shared/back-button";
 import SectionTitle from "@/components/shared/section-title";
+import getCurrentUser from "@/lib/auth/get-current-user";
+import { redirect } from "next/navigation";
 
 interface IProps {
     children: React.ReactNode;
 }
 
-export default function DetailsLayout({ children }: IProps) {
+export default async function DetailsLayout({ children }: IProps) {
+    const user = await getCurrentUser()
+
+    if (!user) {
+        redirect('/auth')
+    }
+
     return (
         <div className="flex flex-col gap-5">
             <div className="flex gap-3 lg:flex-row items-center lg:gap-4 px-3 md:px-0!">
